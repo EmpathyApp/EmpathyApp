5ac787583cb4360e7db163d2b0051189b17fbe7b
SunyataZero <tord.dellsen@gmail.com>
2015-01-03 19:00:21 +0100

    clean up before "early code review" from @sebaste

--- pages/thank_you.php
+++ pages/thank_you.php
@@ -1,19 +1,20 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * License: GPLv3
+ * 
+ * php code for making the actual charge of the credit card. Also displays the
+ * page that the user comes to after making the donation
  */
 
 
-//preheader - used for loading the stripe lib
+// preheader
 function ea_preheader(){
     if( !is_admin() ){
         global $post, $current_user;
         if( !empty( $post->post_content ) && strpos( $post->post_content, "[ea_thankyou]" ) !== false ){
             
-            
+            // Loading the stripe lib
             chdir(dirname(__FILE__));
             require_once('../includes/lib/Stripe.php');
             
@@ -24,62 +25,58 @@ function ea_preheader(){
 add_action('wp', 'ea_preheader', 1);
 
 
-
-
-//Shortcode for the thank you! page
+// Shortcode for the thank you! page
 function ea_thankyou_shortcode(){
     ob_start();
 
-
-echo "debug1";
-var_dump($_POST);
-
-/*
-The Stripe functions use the Stripe php library which is included in public_html/empathy/wp/composer.json
-*/
-
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here https://dashboard.stripe.com/account
-Stripe::setApiKey("sk_test_uEGJelp5bfMDnLp0LSfb9E7N");
-
-echo "debug2";
-
-// Get the credit card details submitted by the form
-$token = $_POST['stripeToken'];
-$amount = 1100;
-$descr = "test description";
-
-echo "debug3";
-
-
-
-// Create the charge on Stripe's servers - this will charge the user's card
-try {
-    echo "debug4";
-    //$amount32 = 32;
-    //var_dump($amount32);
-    var_dump($_POST);
-    //print_r($amount);
-    //echo "Experimenting";
-    $charge = Stripe_Charge::create(array(
-      "amount" => $amount, // amount in cents, again
-      "currency" => "usd",
-      "card" => $token,
-      "description" => $descr)
-    );
-} catch(Stripe_CardError $e) {
-    // The card has been declined
-    echo "<h4>Card has been declined</h3>";
-    echo "debug5";
-}
-
-echo "<h3>Success! Charged $$amount cents</h3>";
+    
+    // php code inserted with shortcode ++++++++++++++++++++++++++++++++++++++++
+    
+    
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here https://dashboard.stripe.com/account
+    Stripe::setApiKey("sk_test_uEGJelp5bfMDnLp0LSfb9E7N");
+    // Get the credit card details submitted by the form
+    $token = $_POST['stripeToken'];
+    $amount = 1100; // TODO: Change to dynamic
+    /*
+     * -please note that (1) the amound is given in cents and (2) the amount is
+     * given two times, once on the client side and also here on the server side
+     * and the amount is not transferred automatically (as long as we use the
+     * "custom" https://stripe.com/docs/checkout#integration-custom checkout
+     * button) which means that we can see a value in the stripe dialog which
+     * gives one value and then another value will be charged from the user's
+     * credit card
+     */
+    $descr = "test description"; // TODO: remove?
+    // Create the charge on Stripe's servers - this will charge the user's card
+    try {
+        $charge = Stripe_Charge::create(array(
+          "amount" => $amount, // amount in cents, again
+          "currency" => "usd",
+          "card" => $token,
+          "description" => $descr)
+        );
+    } catch(Stripe_CardError $e) {
+        // The card has been declined
+        echo "<h4>Card has been declined</h3>";
+    }
+    echo "<h3>Success! Charged $$amount cents</h3>";
 
 
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    
 
     $tmp_content = ob_get_contents();
     ob_end_clean();
     return $tmp_content;
 }
+/*
+ * Create shortcode for the thank you page
+ * First argument is the name of the shortcode so it will be used like this
+ * [ea_donation] on a wp page
+ * Second argument is the name of the php function above which will be used
+ * to insert text into the web page
+ */
 add_shortcode('ea_thankyou', 'ea_thankyou_shortcode');
 ?>
