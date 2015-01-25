7e788c87d0500538550500c78d0d59e2d7db8cf2
Sebaste <sebastian.herbertsson@gmail.com>
2015-01-25 20:04:02 +0000

    Clean-up of file

--- pages/donation-form_sc.php
+++ pages/donation-form_sc.php
@@ -2,24 +2,15 @@
 /*
  * License: GPLv3
  * 
- * Script for handling of donation button and communication with Stripe.
- * After user clicks donate in the pop-up window she will be redirected to
- * another php page
+ * Script for handling the donation button and communication with Stripe.
+ * After the user clicks on "donate" in the pop-up window, she will be redirected to another PHP page.
  * 
- * PLEASE NOTE: The amount is sent unencrypted, so ssl/https is strongly
- * prefered
- * 
- * TODO Ceate constants for these:
- * http://empathy.ihavearrived.org/wp/home/thank-you/
- * stripe public key
- * 
- * TODO Use built-in wp jquery (jQuery UI as well?)
+ * PLEASE NOTE: The amount is sent unencrypted, so SSL/HTTPS is strongly preferred.
  */
 
 function ea_donation_form_shortcode() {
     ob_start(); //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ?>
-
 
     <p>
         <label for="amountDollars">Donation Amount:</label>
@@ -39,9 +30,9 @@ function ea_donation_form_shortcode() {
         };
             
         var tScaleNr = 1;
+        
         var tInitialDonationAmount = getInitialDonationAmount();
-        jQuery(function() {
-            // Setup for jQuery UI slider
+        jQuery(function () {
             jQuery("#sliderDollars").slider({
                 value: tInitialDonationAmount,
                 min: gConst.minDonationDollars,
@@ -49,31 +40,28 @@ function ea_donation_form_shortcode() {
                 step: gConst.donationStepSizeDollars,
                 slide: slideFunction
             });
-            // Showing the initial value as text
+            // Display the initial value as text.
             jQuery("#amountDollars").val("$" + jQuery("#sliderDollars").slider("value"));
         });
         
+        // Event handling for when the user drags slider.
         function slideFunction(iEvent, iUi) {
-            //..event handling for when user drags slider
-            jQuery("#amountDollars").val("$" + iUi.value); //-Issue #17
+            jQuery("#amountDollars").val("$" + iUi.value);
             tScaleNr = iUi.value / tInitialDonationAmount;
+            // By default, Raphael will scale the vector image from its midpoint and outwards. By setting
+            // the last two attribute values to 0, the scaling will be made from the top-left point towards
+            // the bottom-right instead. 
             tHeartSet.attr({"transform": "S" + tScaleNr + "," + tScaleNr + ",0,0"});
-            /*
-             * -the last two values (at the time of writing zeroes)
-             * determine the point relative to which the scaling will be done
-             */
-            if(iUi.value >= gConst.maxDonationDollars - gConst.animationMargin){ //-adding to the margin
+            if (iUi.value >= gConst.maxDonationDollars - gConst.animationMargin) { // Adding to the margin.
                 startAnim();
             }
         }
 
-        function startAnim(){
+        function startAnim() {
             var tAnim = Raphael.animation({'transform':"S" + tScaleNr * 1.1 + "," + tScaleNr * 1.1 + ",0,0"}, 400,
-                function(){
-                    /* 
-                     * Putting the lines below inside this callback makes sure that
-                     * the system will execute them after the first animation is completed
-                     */
+                function () {
+                    // Putting the lines below inside this callback makes sure that the system
+                    // will execute them after the first animation is completed.
                     var tAnim = Raphael.animation({'transform':"S" + tScaleNr + "," + tScaleNr + ",0,0"}, 600);
                     tHeartSet.animate(tAnim);
                 }
@@ -81,10 +69,7 @@ function ea_donation_form_shortcode() {
             tHeartSet.animate(tAnim);
         }
 
-        /*
-         * Gives an inital donation amount which can come from the url parameter
-         * or otherwise from a constant value
-         */
+        // Gives an initial donation amount either from the URL parameter or from a constant value.
         function getInitialDonationAmount() {
             var rVal = getUrlParamValue(gConst.recDonationDollarsUrlParamName);
             if (rVal === null) {
@@ -93,23 +78,20 @@ function ea_donation_form_shortcode() {
             return rVal;
         }
 
-        /*
-         * Takes the name of an url parameter (normally coming from an email
-         * sent to the user) this function gives the associated value
-         */
+        // Given the name of an URL parameter (normally coming from an email sent to the user),
+        // return its associated value.
         function getUrlParamValue(iParamName) {
-            // Grab the string after the question mark
+            // Get the string after the question mark.
             var tUrlVarsString = window.location.search.split('?')[1];
             if (tUrlVarsString === undefined) {
                 return null;
             }
-            // If there are several parameters, split into array
+            // If there are several parameters, split into an array.
             var tUrlVarsArray = tUrlVarsString.split('&');
             for (var i = 0; i < tUrlVarsArray.length; i++) {
-                // Separate name from value
+                // Separate the name from the value.
                 var tParamNameAndValueArray = tUrlVarsArray[i].split('=');
                 if (tParamNameAndValueArray[0] === iParamName) {
-                    // Return value
                     return tParamNameAndValueArray[1];
                 }
             }
@@ -117,30 +99,24 @@ function ea_donation_form_shortcode() {
         }
     </script>
 
-
-
-    
-
     <form id="stripeForm" action=<?php echo getBaseUrl() . pages::donation_sent; ?> method="POST">
         <script src="http://checkout.stripe.com/checkout.js"></script>
         <button id="customButton">Donate</button>
-        <!-- -html for the custom stripe button needs to be placed before the script (why?) -->
         <script>
-        // Checkout on button click..
+        // Checkout on button click.
         jQuery(function () {
             jQuery('#customButton').on('click', function (e) {
-                //..get the amount from the slider
+                // Get the amount from the slider.
                 var tAmount = 100 * jQuery("#sliderDollars").slider("value");
-                // ..open stripe dialog
+                // Open Stripe dialogue.
                 StripeCheckout.open({
                     key: '<?php echo get_private_stripe_key(); ?>',
                     image: '/square-image.png',
                     token: function (responseToken) {
-                        // ..submit the token that we get back from the stripe server to _our_ server
+                        // Submit the token that we get back from the Stripe server to _our_ server.
                         var tokenInput = jQuery('<input type=hidden name=stripeToken />').val(responseToken.id);
-                        //-(the token is used on the server side to create the actual charge)
-                        //jQuery('#stripeForm').append(tokenInput).submit();
-                        var tAmountStr = jQuery('<input type=hidden name=amountCents />').val( tAmount );
+                        // (The token is used on the server side to create the actual charge).
+                        var tAmountStr = jQuery('<input type=hidden name=amountCents />').val(tAmount);
                         jQuery('#stripeForm').append(tokenInput).append(tAmountStr).submit();
                     },
                     name: 'Demo Site',
@@ -149,7 +125,7 @@ function ea_donation_form_shortcode() {
                 });
                 e.preventDefault();
             });
-            // Close Checkout on page navigation
+            // Close checkout on page navigation.
             jQuery(window).on('popstate', function () {
                 handler.close();
             });
@@ -157,9 +133,10 @@ function ea_donation_form_shortcode() {
         </script>
     </form>
 
-    <!-- Dynamically updated image connected to the donation slider -->
+    <!-- Dynamically updated vector image connected to the donation slider. -->
     <div id="container"></div>
-    <script> // svg image made with inkscape and http://readysetraphael.com/
+    <script>
+        // SVG image made with Inkscape and http://readysetraphael.com/.
         var paper = Raphael('container', '310', '310'); //'121', '99'
         var path3053 = paper.path("");
         path3053.attr({id: 'path3053',fill: '#000000','stroke-width': '0','stroke-opacity': '1'}).data('id', 'path3053');
@@ -170,20 +147,15 @@ function ea_donation_form_shortcode() {
         var path4100 = paper.path("M 27.696928,76.261018 C 19.747206,68.626322 11.266398,60.587164 7.4748915,50.835542 3.6833851,41.083922 3.3734124,26.811381 9.7549518,17.653528 16.136491,8.4956721 22.187901,4.6870338 32.951844,3.819554 43.715784,2.9520766 54.370274,9.8559705 58.825976,15.322773 63.03182,9.0752373 73.149817,2.1597917 82.97532,2.535815 c 9.825503,0.3760235 17.58955,4.1161266 24.45634,14.16105 6.86678,10.04492 6.42359,25.935606 2.63918,34.821536 C 106.2864,60.40433 95.625874,73.161814 87.142497,79.412544 78.659121,85.663272 67.21867,93.59175 59.402765,95.98188 51.60661,93.099221 35.646649,83.895715 27.696928,76.261018 z");
         path4100.attr({id: 'path4100',fill: '#ff3e35',stroke: '#000000',"stroke-width": '5',"stroke-linecap": 'butt',"stroke-linejoin": 'miter',"stroke-opacity": '1',"stroke-miterlimit": '4',"stroke-dasharray": 'none',"fill-opacity": '1'}).data('id', 'path4100');
         var tHeartSet = paper.set(path3053, path3051, path3049, path4100);
-        //tHeartSet.attr({"fill": "green"});
     </script>
 
-    
     <?php
     $ob_content = ob_get_contents(); //+++++++++++++++++++++++++++++++++++++++++
     ob_end_clean();
     return $ob_content;
 }
-/*
- * Create shortcode for the caller donation page
- * First argument is the name of the shortcode so it will be used like this
- * [ea_donation] on a wp page
- * Second argument is the name of the php function above which will be used
- * to insert text into the web page
- */
+
+// Create shortcode for the caller donation page.
+// The 1st argument is the name of the shortcode, meaning that it will be used as "[<NAME>]" on a WP page.
+// The 2nd argument is the name of the PHP function above, which will be used to insert text into the webpage.
 add_shortcode('ea_donation_form', 'ea_donation_form_shortcode');
