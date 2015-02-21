b8154c39a5ad679712d678a5245cf0db10f5d7a1
SunyataZero <tord.dellsen@gmail.com>
2015-02-21 20:04:27 +0100

    improved email-form by adding checks against input we want to have 1. numbers 2. positive numbers (different error messages are given depending on which of these two cases happens)

--- pages/email-form_sc.php
+++ pages/email-form_sc.php
@@ -32,10 +32,13 @@ function ea_email_form_shortcode() {
         type="hidden"
         method="POST"
     >
-        <label for="skype_name">Skype Name</label><input name="skype_name" id="skype_name" type="text"><br>
-        <label for="skype_name">Call Duration</label><br>
-        <input name="length" id="length" type="number"><br>
-        <input type="submit" value="Send" id="submit"><br>
+        <label for="skype_name">Skype Name</label><input name="skype_name" id="skype_name" type="text">
+        <br>
+        <label for="skype_name">Call Duration</label>
+        <br>
+        <input name="length" id="length" type="number" pattern="\d*" min="1">
+        <br><br>
+        <input type="submit" value="Send" id="submit">
     </form>
 
 
