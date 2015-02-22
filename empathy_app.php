74d356052f46436c009b4b15a301036d355d1adb
sebaste <sebastian.herbertsson@gmail.com>
2015-02-22 18:58:44 +0100

    Change PHP error_reporting value; #72

--- empathy_app.php
+++ empathy_app.php
@@ -37,7 +37,7 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(-1);
+ini_set('error_reporting', E_ALL);
 
 //##############################################################################
 
